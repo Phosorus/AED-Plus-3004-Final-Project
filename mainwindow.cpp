6b162e263619b57bc34d8c67d77ce43f69b39a06
Erictran004 <56602588+Erictran004@users.noreply.github.com>
2023-12-06 16:22:19 -0500

    Added the UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,179 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+/*
+ * Need to create a function that cycles through the AED Process (probably in the AED controller)
+*/
+void MainWindow::on_btnPowerButton_clicked()
+{
+    /*
+    Need to check power level
+    Only after the power is on can you use the unit
+    Second step is to check status of AED
+    */
+
+    /*if power level not sufficient indicate audio msg
+
+    QString s = "Change batteries";
+    s = "\"" + s +"\"";
+    s = "AED: " + s;
+    ui->ActionLog->append(s);
+
+    */
+}
+
+void MainWindow::on_btnShockIndicator_clicked()
+{
+    //Using this as a testing function for now
+    counter++;
+    stepIndicator(counter);
+    if(counter == 5){
+        counter = 0;
+    }
+    /*
+    Need to add colour changing for the ready charge and probably audio queue
+    */
+}
+
+void MainWindow::on_btnStatus_clicked()
+{
+    //Administer self test
+
+    //if unit is good to go
+
+    QString s = "Automatic Defibrilator. Unit OK!";
+    s = "\"" + s +"\"";
+    s = "AED: " + s;
+    ui->ActionLog->append(s);
+
+    //if unit is not good
+    //output indicator
+}
+void MainWindow::stepIndicator(int i){
+    if(i==1){
+        for(int y =0; y<1000; y++){
+
+        }
+        ui->lblStep1->setStyleSheet("border: 4px solid  rgb(45, 255, 0); border-radius: 6px;");
+        ui->lblStep2->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep3->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep4->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep5->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep6->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        QString s = "Check responsiveness";
+        s = "\"" + s +"\"";
+        s = "AED: " + s;
+        ui->ActionLog->append(s);
+    }
+    else if(i==2){
+        ui->lblStep2->setStyleSheet("border: 4px solid  rgb(45, 255, 0); border-radius: 6px;");
+        ui->lblStep1->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep3->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep4->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep5->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep6->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        QString s = "Call for help";
+        s = "\"" + s +"\"";
+        s = "AED: " + s;
+        ui->ActionLog->append(s);
+    }
+    else if(i==3){
+        ui->lblStep3->setStyleSheet("border: 4px solid  rgb(45, 255, 0); border-radius: 6px;");
+        ui->lblStep2->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep1->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep4->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep5->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep6->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        QString s = "Attach defib pads to patient's bare chest";
+        s = "\"" + s +"\"";
+        s = "AED: " + s;
+        ui->ActionLog->append(s);
+    }
+    else if(i==4){
+        ui->lblStep4->setStyleSheet("border: 4px solid  rgb(45, 255, 0); border-radius: 6px;");
+        ui->lblStep2->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep3->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep1->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep5->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep6->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        QString s = "Dont't touch patient! Analyzing";
+        s = "\"" + s +"\"";
+        s = "AED: " + s;
+        ui->ActionLog->append(s);
+    }
+    else if(i==5){
+        ui->lblStep5->setStyleSheet("border: 4px solid  rgb(45, 255, 0); border-radius: 6px;");
+        ui->lblStep6->setStyleSheet("border: 4px solid  rgb(45, 255, 0); border-radius: 6px;");
+        ui->lblStep2->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep3->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep4->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        ui->lblStep1->setStyleSheet("border: 4px solid  rgb(175, 193, 204); border-radius: 6px;");
+        QString s = "Start CPR";
+        s = "\"" + s +"\"";
+        s = "AED: " + s;
+        ui->ActionLog->append(s);
+    }
+}
+
+
+void MainWindow::on_btnCheckPatient_clicked()
+{
+    QString s = "Are you alright?";
+    s = "\"" + s +"\"";
+    s = "USER to PATIENT: " + s;
+    ui->ActionLog->append(s);
+}
+
+
+void MainWindow::on_btnCallHelp_clicked()
+{
+    QString s = "Calls for help";
+    s = "*" + s +"*";
+    s = "USER: " + s;
+    ui->ActionLog->append(s);
+}
+
+
+void MainWindow::on_btnApplyCompressions_clicked()
+{
+    QString s = "Applies 20 chest compressions";
+    s = "*" + s +"*";
+    s = "USER: " + s;
+    ui->ActionLog->append(s);
+    //Somehow determine compression quality and have AED give feedback
+}
+
+void MainWindow::on_btnApplyBreathes_clicked()
+{
+    QString s = "Applies 2 breathes";
+    s = "*" + s +"*";
+    s = "USER: " + s;
+    ui->ActionLog->append(s);
+}
+
+
+void MainWindow::on_btnAttachAdultPads_clicked()
+{
+    ui->lblAdult->setStyleSheet("font-weight: bold; font-size: 18px; color: rgb(45, 255, 0);");
+    ui->lblChild->setStyleSheet("font-weight: bold; font-size: 18px; color: rgb(255, 255, 255);");
+    QString s = "Attaches adult pads to patient";
+    s = "*" + s +"*";
+    s = "USER: " + s;
+    ui->ActionLog->append(s);
+
+    //Maybe update the value of the AED class to reflect what pad is attached
+}
+
+
+void MainWindow::on_btnAttachChildPads_clicked()
+{
+    ui->lblAdult->setStyleSheet("font-weight: bold; font-size: 18px; color: rgb(255, 255, 255);");
+    ui->lblChild->setStyleSheet("font-weight: bold; font-size: 18px; color: rgb(45, 255, 0);");
+    QString s = "Attaches child pads to patient";
+    s = "*" + s +"*";
+    s = "USER: " + s;
+    ui->ActionLog->append(s);
+
+    //Maybe update the value of the AED class to reflect what pad is attached
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <random>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,8 +15,27 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void stepIndicator(int step);
+
+private slots:
+    void on_btnPowerButton_clicked();
+    void on_btnStatus_clicked();
+    void on_btnShockIndicator_clicked();
+
+    void on_btnCheckPatient_clicked();
+    void on_btnCallHelp_clicked();
+
+
+    void on_btnApplyCompressions_clicked();
+
+    void on_btnApplyBreathes_clicked();
+
+    void on_btnAttachAdultPads_clicked();
+
+    void on_btnAttachChildPads_clicked();
 
 private:
     Ui::MainWindow *ui;
+    int counter;
 };
 #endif // MAINWINDOW_H
