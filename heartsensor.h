9e9a4f54d9c04a0f0381fbe59a9395b2229874b8
A.J. Bailey <132167988+Phosorus@users.noreply.github.com>
2023-12-11 17:06:21 -0500

    Update heartsensor.h
    Removed signals and slots, weren't used

--- heartsensor.h
+++ heartsensor.h
@@ -15,15 +15,6 @@ public:
     HeartSensor(Patient* patient);
     bool getWorking(); // check if this part is working
     int getPatientCondition(); //get patient condition
-
-public slots:
-    //int getCondition(int i);
-
-signals:
-    void sendPEASignal(); // send PEA signal
-    void sendAsystoleSignal(); // send Asystole Signal
-    void sendVTSignal(); // send VT signal
-    void sendVFSignal(); // send VF signal
 };
 
 #endif // HEARTSENSOR_H
